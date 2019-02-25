fe0f320c9895e2b519bee020307c05bec0e5ea2e
Ujjwal's iMac <redeem@Redeems-iMac.local>
2019-02-25 13:59:26 +0545

    Ädded test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Ujjwal's iMac on 2/25/19.
+//  Copyright © 2019 Redeem. All rights reserved.
+//
+
+import Foundation
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SwiftyLibTests/SwiftyLibTests.swift
+++ SwiftyLibTests/SwiftyLibTests.swift
@@ -10,13 +10,23 @@ import XCTest
 @testable import SwiftyLib
 
 class SwiftyLibTests: XCTestCase {
+    
 
+    var swiftyLib: SwiftyLib!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = SwiftyLib()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    func testAdd(){
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
+    func testSub(){
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
 
     func testExample() {
